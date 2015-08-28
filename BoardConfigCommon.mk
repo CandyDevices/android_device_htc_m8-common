e941722d42598faa149d1360e0e62d6c537fc5e9
rc420head <dennislebhardt@yahoo.com>
2015-08-27 23:21:43 -0500

    Add The Sweetness (1/2)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,18 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
 TARGET_KERNEL_SOURCE := kernel/htc/msm8974
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-candy-linux-gnueabi/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-CANDY-linux-gnueabi-
+
+# Rom Optimization
+USE_GRAPHITE := true
+USE_FSTRICT_FLAGS := true
+FSTRICT_ALIASING_WARNING_LEVEL := 3
+USE_BINARY_FLAGS := true
+USE_EXTRA_CLANG_FLAGS := true
+USE_LTO := true
+CANDY_ARM_OPT_LEVEL := -O3
+CANDY_THUMB_OPT_LEVEL := -O3
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
